210ab5fdffc357a4149b8d245afadfbd524c3051
Terezinha Freire C Sousa <terezinhafcs@id.uff.br>
2025-03-01 18:50:59 -0300

    Add files via upload

---
+++ appdia4.js
@@ -0,0 +1,33 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio Rafa Ballerini.';
+
+console.log('ola');
+let contador = 2;
+let acertouNumero = Math.floor(Math.random() * 7 +1);
+let chute = 0;
+
+while (chute != 7){
+  
+  chute = prompt ('Qual o valor que você deseja chutar?');
+  if (chute == acertouNumero){
+    alert('Parabéns você acertou o número ');
+    break 
+  }
+  
+  chute = prompt('Chute outro valor');
+
+  if (chute == acertouNumero){
+    alert('Parabéns você acertou o número ');
+    break
+  }
+
+
+  chute = prompt('Chute outro valor');
+  if (chute == acertouNumero){
+    alert('Parabéns você acertou o número ');
+    break
+  } 
+
+  
+  else{alert(`Que pena não foi dessa vez, o número era o ${acertouNumero}`);contador = 3;}
+}
